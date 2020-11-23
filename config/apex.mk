ce8b5f4356d6eed97acdce7a24c11a576ad8c5b3
Henrique Silva <jhenrique09.mcz@hotmail.com>
2020-11-23 16:33:20 +0900

    vendor: Enable apex only for supported devices
    Signed-off-by: Daryl Ronningen <relms@relms.dev>

--- config/apex.mk
+++ config/apex.mk
@@ -13,6 +13,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+ifeq ($(TARGET_FLATTEN_APEX),false)
+
+# Overlays
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    vendor/aniosp/apex/overlay
+
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/aniosp/apex/overlay/common
 
 # Enable Google Play system updates support
 PRODUCT_SOONG_NAMESPACES += \
@@ -25,18 +33,20 @@ PRODUCT_PACKAGES += \
 # Google Apexes
 PRODUCT_PACKAGES += \
     com.google.android.adbd \
-	com.google.android.cellbroadcast \
-	com.google.android.conscrypt \
-	com.google.android.extservices \
-	com.google.android.ipsec \
-	com.google.android.media \
-	com.google.android.media.swcodec \
-	com.google.android.mediaprovider \
-	com.google.android.neuralnetworks \
-	com.google.android.os.statsd \
-	com.google.android.permission \
-	com.google.android.resolv \
-	com.google.android.sdkext \
-	com.google.android.telephony \
-	com.google.android.tzdata2 \
-	com.google.android.wifi
+    com.google.android.cellbroadcast \
+    com.google.android.conscrypt \
+    com.google.android.extservices \
+    com.google.android.ipsec \
+    com.google.android.media \
+    com.google.android.media.swcodec \
+    com.google.android.mediaprovider \
+    com.google.android.neuralnetworks \
+    com.google.android.os.statsd \
+    com.google.android.permission \
+    com.google.android.resolv \
+    com.google.android.sdkext \
+    com.google.android.telephony \
+    com.google.android.tzdata2 \
+    com.google.android.wifi
+
+endif
