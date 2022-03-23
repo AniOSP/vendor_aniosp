9e32b6c7c3e10e089d230565edf3df7de44452b9
Danny Trunk <dtrunk90@gmail.com>
2022-03-23 22:28:02 +0100

    soong: Add TARGET_USES_EGL_DISPLAY_ARRAY conditional
    This soong variable is used to conditionally revert a commit [1]
    which causes random camera crashes on tama devices
    
    [1] https://github.com/LineageOS/android_frameworks_native/commit/a9550f3fe9097e0934e9b44c5aac6b914fb46aec
    
    Change-Id: I749c7029f8f2b6d6d95b066aed4929c33e3c75e7
    Signed-off-by: Daryl Ronningen <relms@relms.dev>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -42,6 +42,8 @@ SOONG_CONFIG_customGlobalVars += \
     target_uses_prebuilt_dynamic_partitions \
     uses_camera_parameter_lib \
     uses_legacy_fd_fbdev
+    uses_camera_parameter_lib \
+    uses_egl_display_array
 
 SOONG_CONFIG_NAMESPACES += customNvidiaVars
 SOONG_CONFIG_customNvidiaVars += \
@@ -81,6 +83,7 @@ SOONG_CONFIG_customQcomVars_supports_hw_fde_perf := $(TARGET_HW_DISK_ENCRYPTION_
 SOONG_CONFIG_customQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_customQcomVars_uses_qcom_bsp_legacy := $(TARGET_USES_QCOM_BSP_LEGACY)
 SOONG_CONFIG_customGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDEV)
+SOONG_CONFIG_customGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
