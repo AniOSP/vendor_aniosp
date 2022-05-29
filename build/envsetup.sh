Evolution-X-AOSP/vendor_evolution

vendor: Inline versioning to Google
* As pointed out in partner

Change-Id: Id35912ecb0997adc1b61d06a3e39e2198d5c5b8f
Signed-off-by: Akito Mizukito <akito@evolution-x.org>

build/envsetup.sh
@@ -55,7 +55,7 @@ function brunch()
 {
     breakfast $*
     if [ $? -eq 0 ]; then
-        mka bacon
+        mka evolution
     else
         echo "No such item in brunch menu. Try 'breakfast'"
         return 1
@@ -666,7 +666,7 @@ function cmka() {
     if [ ! -z "$1" ]; then
         for i in "$@"; do
             case $i in
-                bacon|otapackage|systemimage)
+                evolution|otapackage|systemimage)
                     mka installclean
                     mka $i
                     ;;

AniOSP/vendor_aniosp

Signed-off-by: Daryl Ronningen <relms@relms.dev>

build/envsetup.sh
@@ -55,7 +55,7 @@ function brunch()
 {
     breakfast $*
     if [ $? -eq 0 ]; then
-        mka bacon
+        mka aniosp
     else
         echo "No such item in brunch menu. Try 'breakfast'"
         return 1
@@ -666,7 +666,7 @@ function cmka() {
     if [ ! -z "$1" ]; then
         for i in "$@"; do
             case $i in
-                bacon|otapackage|systemimage)
+                aniosp|otapackage|systemimage)
                     mka installclean
                     mka $i
                     ;;